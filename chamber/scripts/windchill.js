25a346af37bbb60009ee51293f1f40525f5e7043
Rfemerson <emersonferreirar8@gmail.com>
2023-06-04 00:51:12 -0300

    Fix chamber site

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,10 +1,10 @@
-function calculateWindChill(temperature, windSpeed) {
-    if (temperature <= 50 && windSpeed > 3.0) {
-      let windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
-      
-      return windChill.toFixed(2);
-    } else {
-      return "N/A"; 
-    }
-  }
-  
+let temp = document.querySelector("#temperature").innerHTML;
+let windSpeed = document.querySelector("#windSpeed").innerHTML;
+
+if (temp <= 10 && windSpeed >= 4.8) {
+	const windChill = 13.2 + (0.6215 * temp) - (11.37 * windSpeed ** 0.16) + (0.3965 * temp * windSpeed ** 0.16);
+	document.querySelector("#windChill").innerHTML = Math.round(windChill) + " °C";
+}
+else {
+	document.querySelector("#windChill").innerHTML = "N/A"
+}
