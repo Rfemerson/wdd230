583c1403a4b8416d7229650ad2487fd9826cfc5b
Rfemerson <emersonferreirar8@gmail.com>
2023-06-15 21:35:27 -0300

    Finishe Join us Page

--- Lesson09/scripts/prophets.js
+++ Lesson09/scripts/prophets.js
@@ -17,19 +17,32 @@ const displayProphets = (prophets) => {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let portrait = document.createElement('img');
-        let p = document.createElement('p');
+        let dateOfBirth = document.createElement('p');
+        let placeOfBirth = document.createElement('p');
+        let children = document.createElement('p');
+        let dateOfDeath = document.createElement('p');
+        // let placeOfBirth = document.createElement('p');
+
 
         h2.textContent = `${prophet.name} ${prophet.lastname}`;
-        p.textContent = `Date of Birth: ${prophet.birthdate} Place of Birth: ${prophet.birthplace}`;
+        dateOfBirth.textContent = `Date of Birth: ${prophet.birthdate}`;
+        placeOfBirth.textContent = `Place of Birth: ${prophet.birthplace}`;
+        children.textContent = `Children: ${prophet.numofchildren}`;
+        dateOfDeath.textContent = `Death: ${prophet.death}`;
 
         portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', 'Portrait of ${prophet.name}');
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-day President`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
         
         card.appendChild(h2);
-        card.appendChild(p)
+        card.appendChild(dateOfBirth);
+        card.appendChild(placeOfBirth);
+        card.appendChild(children);
+        if (prophet.death != null) {
+            card.appendChild(dateOfDeath);
+        };
         card.appendChild(portrait);
 
         cards.appendChild(card);
