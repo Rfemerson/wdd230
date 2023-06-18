f02062c6aa0da66a2d4b5cef5a7e497cb1688832
Rfemerson <emersonferreirar8@gmail.com>
2023-06-18 01:55:12 -0300

    Directory page

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,43 @@
+const path = 'scripts/data.json'
+
+async function getCompanyData() {
+    const response = await fetch(path);
+    const data = await response.json();
+    console.table(data.companies);
+    displayCompanies(data.companies)
+}
+
+getCompanyData();
+
+const displayCompanies = (companies) => {
+    const business = document.querySelector("div.company");
+
+    companies.forEach((company) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let logoImg = document.createElement('img');
+        let address = document.createElement('p');
+        let contact = document.createElement('p');
+        let webSite = document.createElement('p');
+
+
+        name.textContent = `${company.name}`;
+        address.textContent = `${company.address}`;
+        contact.textContent = ` ${company.contact}`;
+        webSite.textContent = `Death: ${company.link}`;
+
+        logoImg.setAttribute('src', company.logoPath);
+        logoImg.setAttribute('alt', `logoImg of ${company.name}`);
+        logoImg.setAttribute('loading', 'lazy');
+        logoImg.setAttribute('width', '300');
+        logoImg.setAttribute('height', '300');
+        
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(contact);
+        card.appendChild(logoImg);
+        card.appendChild(webSite);
+
+        business.appendChild(card);
+    });
+}
